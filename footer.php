a74e4720a9e2de430570c928ca127c253cb81f25
Олександр Юношев <a.a.yunoshev@ukr.net>
2020-11-20 11:06:50 +0200

    Лаб 4

---
+++ footer.php
@@ -0,0 +1,21 @@
+
+<div class="footer">
+					<div class="footerTop">
+						<img src="img/arcBig.png" alt="arcBig" id="arcfooter">
+						<p id="footerTopLabel">Spotlight Features</p>
+					</div>
+					<div class="footerBottom">
+						<img src="img/Layer9.png" alt="Layer9" id="Layer9">
+						<p id="footerp1">Seminars in your area</p>
+						<p id="footerp2">Dates, topics, locations,</p>
+						<p id="footerp3">& prices. Save 10% when</p>
+						<p id="footerp4">3 or more sign-up!</p>
+
+						<p id="footerp5">Topics, Trends,</p>
+						<p id="footerp6">Techniques</p>
+						<p id="footerp7">Don't let your patients</p>
+						<p id="footerp8">give you the brush-off.</p>
+						<p id="footerp9">Increase patient</p>
+						<p id="footerp10">acceptance!</p>
+					</div>
+</div>
---
+++ lab4.php
@@ -0,0 +1,172 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<title>Yunoshev_lab4</title>
+	<link rel="stylesheet" href="style.css">
+	
+</head>
+
+<body>
+	<div class="body">
+
+		<?php include('header.php');?>
+		<div class="contentLeft">
+			<div class="contentLeftRight">
+				<div id="Link1">
+					<hr class="hr1">
+					<hr class="hr6">
+					<hr class="hr7">
+					<hr class="hr8">
+					<a href="index.html" class="linka" id="linka">Главная</a>
+				</div>
+
+				<div class="linkDiv">
+					<hr class="hr2">
+					<hr class="hr3">
+					<hr class="hr4">
+					<hr class="hr5">
+					<a href="form_input.html" class="linka">Регистрация</a>
+				</div>
+
+				<div class="linkDiv">
+					<hr class="hr2">
+					<hr class="hr3">
+					<hr class="hr4">
+					<hr class="hr5">
+					<a href="form_link.html" class="linka">Перенаправление</a>
+				</div>
+
+				<div class="linkDiv">
+					<hr class="hr2">
+					<hr class="hr3">
+					<hr class="hr4">
+					<hr class="hr5">
+					<a href="#" class="linka" onclick="anim()">Анимация</a>
+				</div>
+
+                <?php include('footer.php');?>
+				
+
+			</div>
+		</div>
+
+		<form class="contentRight" method="post" action="">
+			<div class="contentRightTop">
+				<p class="CRTlabel">Регистрация</p>
+			</div>
+
+
+			<div class="contentName">
+				<p id="p-name">Имя: </p>
+				<input type="text" id="input-name" name="input-name">
+			</div>
+
+			<div class="contentSex">
+				<p id="p-sex">Пол: </p>
+				<input type="radio" id="input-male" name="sex" checked="true" value="man"> Мужской
+				<input type="radio" id="input-female" name="sex" value="woman"> Женский
+			</div>
+
+			<div class="contentDateOfBirth">
+				<div> 
+					<p id="p-day">День: </p>
+					<input type="number" id="input-day" name="input-day" min="1" max="31">
+				</div>
+
+				<div>
+					<p id="p-month">Месяц: </p>
+					<input type="number" id="input-month" name="input-month" min="1" max="12">
+				</div>
+
+				<div>
+					<p id="p-year">Год: </p>
+					<input type="number" id="input-year" name="input-year" min="1900" max="2020">
+				</div>
+
+				<button id="btn-reg" type="submit" name="submt">Зарегистрироваться</button>
+
+			</div>
+
+        </form>
+       
+    </div>
+    <div style="margin: auto; width: 200px;">
+        <?php
+                    if(isset($_POST["submt"]))
+                    {
+                        $name = $_POST['input-name'];
+                        $radio = $_POST['sex'];
+                        $day = $_POST['input-day'];
+                        $month = $_POST['input-month'];
+                        $year = $_POST['input-year'];
+                    
+                        $now_year = 2020;
+                        $now_month = 11;
+                        $now_day = 20;
+
+                        $age = $now_year - $year;
+                        if(!empty($name) && !empty($day) && !empty($month) && !empty($year)) 
+                        {
+                            if($radio=="man")
+                            {
+                                if ($age == 21)
+                                {
+                                    if($now_month < $month) 
+                                    {
+                                    $age--;
+                                    }
+                                    else {
+                                        if (($now_day < $day) && $now_month == $month) {
+                                        $age--;
+                                        }
+                                    }
+                                }
+                                if($age >= 21) {
+                                    echo "Регистрация прошла успешно";
+                                }
+                                else {
+                                    echo "Нельзя зарегистрироваться";
+                                }
+                            }
+                            else 
+                            {
+                                if ($age == 18) {
+                                    if($now_month < $month) 
+                                    {
+                                    $age--;
+                                    }
+                                    else 
+                                    {
+                                        if (($now_day < $day) && $now_month == $month) 
+                                        {
+                                        $age--;
+                                        }
+                                    }
+                                }
+                                if($age >= 18) 
+                                {
+                                    echo "Регистрация прошла успешно";
+                                }
+                                else 
+                                {
+                                    echo "Нельзя зарегистрироваться";
+                                }
+                            }
+                    
+                        } 
+                        else 
+                        {
+                            echo "НЕ ХВАТАЕТ ДАННЫХ!!!";
+                        }
+                    }
+                    		
+        ?> 
+    </div>
+</body>
+
+
+ <?php include('lab4_2.php');?>
+</html>
